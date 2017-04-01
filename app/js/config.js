81b8980e2128b21521f6580d8beb57dcf7823b7b
Raya Straus <rcstraus@gmail.com>
2017-04-01 21:46:16 +0300

    first commit

---
+++ app/js/config.js
@@ -0,0 +1,5 @@
+photoAlbumApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
+  cloudinaryProvider
+      .set("cloud_name", "rayastr")
+      .set("upload_preset", "rzayztob");
+}]);
